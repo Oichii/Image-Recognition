171e3d215a5c5fa9cfeaa973abbd184ab82ebbbb
Aleksandra <oichii666@gmail.com>
2019-12-22 09:57:38 +0100

    Dodaj pliki projektów.

---
+++ POBR/POBR.cpp
@@ -0,0 +1,13 @@
+﻿#include<opencv2/opencv.hpp>
+#include<iostream>
+using namespace std;
+using namespace cv;
+int main()
+{
+    Mat img = imread("img.jpg");
+    namedWindow("image_to detect", WINDOW_NORMAL);
+    imshow("image_to_detect", img);
+    waitKey(0);
+
+    return 0;
+}
